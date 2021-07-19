0fa71c54b2adf12855aea1b0d748252de2077bf1
Roney-juma <ochirone10@gmail.com>
2021-07-19 06:30:33 +0300

    updated the meta class and methods

--- awwards/models.py
+++ awwards/models.py
@@ -42,3 +42,95 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title 
+
+
+    class Meta:
+        ordering = ['upload_date'] 
+
+    def save_project(self):
+      '''
+      Saves project instance to db
+      '''
+      self.save()
+
+
+    @classmethod
+    def get_all_projects(cls):
+      '''
+      Returns all project objects from db
+      '''
+      projects=cls.objects.all()
+      return projects 
+
+
+    @classmethod
+    def get_project_by_id(cls,id):
+      '''
+      Returns project based on its id
+      '''
+      project=cls.objects.get(id=id)
+      return project
+
+
+
+    @classmethod
+    def delete_project(cls,id):
+      '''
+      Deletes project based on its id
+      '''
+      cls.objects.filter(id=id).delete()
+
+
+    @classmethod
+    def search_project(cls,prj_title):
+      '''
+      Allows us to search for a project using its title.
+      '''
+      projects=cls.objects.filter(title__icontains=prj_title)
+      return projects
+
+
+
+    @classmethod
+    def filter_by_userid(cls,user_id):
+      """
+      Allows us to filter projects by the user posted.
+      """
+      projects=cls.objects.filter(user_id=user_id)
+      return projects
+
+
+class Rating(models.Model):
+    rating = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6, '6'),
+        (7, '7'),
+        (8, '8'),
+        (9, '9'),
+        (10, '10'),
+    )
+
+    design = models.IntegerField(choices=rating,  blank=True)
+    usability = models.IntegerField(choices=rating, blank=True)
+    content = models.IntegerField(choices=rating, blank=True)
+    score = models.FloatField(blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    upload_date = models.DateTimeField(auto_now_add=True)
+
+
+    def save_rating(self):
+        self.save()
+
+
+    @classmethod
+    def get_project_ratings(cls, id):
+        ratings = Rating.objects.filter(project_id=id).all()
+        return ratings
+
+    def __str__(self):
+        return str(self.id)
