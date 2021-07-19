a17b2c772f165270a24efbc0d9c7b5b1e1d81d2a
Roney-juma <ochirone10@gmail.com>
2021-07-19 06:38:31 +0300

    Created the home page virw

--- awwards/views.py
+++ awwards/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import SignUpForm,ProjectForm,RatingsForm,UserProfileForm
+from django.http  import HttpResponse,Http404,HttpResponseRedirect,JsonResponse
+from django.contrib.auth.decorators import login_required
+from .models import Project,Rating,Profile
+import datetime
+import random
 
 # Create your views here.
+
+def home_page(request):
+    projects=Project.get_all_projects()
+    today = datetime.date.today()
+    ratings= Rating.objects.all()
+    prjs= [p.project for p in ratings]
+    site_of_day=random.choice(prjs)
+    site_of_day_ratings=get_project_ratings_av(site_of_day.id)
