ab4bd141551e1dac529b44fd630ab86536f9bc50
Roney-juma <ochirone10@gmail.com>
2021-07-19 06:39:57 +0300

    defined the Register User view

--- awwards/views.py
+++ awwards/views.py
@@ -17,3 +17,27 @@ def home_page(request):
     prjs= [p.project for p in ratings]
     site_of_day=random.choice(prjs)
     site_of_day_ratings=get_project_ratings_av(site_of_day.id)
+
+    context={
+        'projects':projects,
+        'today':today,
+        'site':site_of_day,
+        'site_ratings':site_of_day_ratings
+    }
+    return render(request,'index.html',context)
+
+
+def register_user(request):
+  if request.method == "POST":
+    form = SignUpForm(request.POST)
+    if form.is_valid():
+        form.save()
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        messages.success(request, f'Account for username {username} successfully created.')
+        return redirect('home_page')
+  else:
+      form = SignUpForm()
+  return render(request, 'registration/registration_form.html', {'form': form})
