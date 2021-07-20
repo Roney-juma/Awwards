794f070056736ceb6c187d9c98f9e1a19d0d9a21
Roney-juma <ochirone10@gmail.com>
2021-07-20 23:15:27 +0300

    added the project form

--- awwards/forms.py
+++ awwards/forms.py
@@ -17,3 +17,19 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username','email', 'password1', 'password2',)
+
+
+class ProjectForm(forms.ModelForm):
+    cover_photo = CloudinaryField('cover_photo',blank=True)
+    # cover_photo = ImageField(label='')
+    url = forms.URLField(label='Live site')
+    widgets = {
+          "title":forms.TextInput(attrs={"class":"form-control mb-4"}),
+          "description":forms.Textarea(attrs={'cols': 110, 'rows': 15,"class":"form-control mb-4"}),
+          "live_site":forms.URLInput(attrs={"class":"form-control mb-4"}),
+          "technologies":forms.TextInput(attrs={"class":"form-control mb-4"}),
+      }
+
+    class Meta:
+        model = Project
+        fields = ('title','description','cover_photo', 'url','technologies')
